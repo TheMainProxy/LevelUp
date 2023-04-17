12d33e171ff225c834ee5a85011cf616a29740ae
TheMainProxy <129001389+TheMainProxy@users.noreply.github.com>
2023-04-17 19:18:05 -0400

    Add files via upload

---
+++ levelup.lua
@@ -0,0 +1,12 @@
+local gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+local old 
+old = hookmetamethod(game,"__index",function(x,y)
+    if checkcaller() and y == "GameId" then
+        if string.find(string.lower(gameName),"bad business") then return 3233893879 
+        elseif string.find(string.lower(gameName),"arsenal") then return 286090429
+        elseif string.find(string.lower(gameName),"aimblox") then return 6808416928 end
+    end 
+    return old(x,y)
+end)
+_G.wl_key="f1b50e5d8f8f47288d87a0e440190f582b2d4c9a"
+loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5062/18849/LevelUpPro.lua'))()
